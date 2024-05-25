cb766c4558a1bbae6ea4ca42a50ded3f7aff77e1
damian.sporin <damian.sporin@study.hs-duesseldorf.de>
2024-05-25 19:31:36 +0200

    Sounds hinzugefügt

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -17,6 +17,7 @@ function simpleParallaxHorizontal(intensity, element) {
     element.css("transform", "translateX(" + imgPos + ")");
   });
 }
+
 function simpleParallaxSidewards(intensityX, intensityY, element) {
   $(window).on("scroll", function () {
     var scrollTop = $(window).scrollTop();
@@ -34,4 +35,48 @@ $(document).ready(function () {
   simpleParallaxSidewards(-2, 4, $(".ShootingStar"));
   simpleParallaxSidewards(4, 4, $(".satellite"));
   simpleParallaxSidewards(2, 2, $(".meteor"));
-});
+
+  
+    // Setup initial volume from the slider
+    var volume = $('#volumeSlider').val();
+    $('audio').each(function() {
+      this.volume = volume;
+    });
+  
+    // Update volume on slider change
+    $('#volumeSlider').on('input', function() {
+      var volume = $(this).val();
+      $('audio').each(function() {
+        this.volume = volume;
+      });
+    });
+  
+    var audioLiftOffPlayed = false;
+    var audioMissionPlayed = false;
+  
+    // Initialisiert den Audio-Start bei der ersten Scroll-Aktion
+    $(window).one('scroll', function () {
+      if (!audioLiftOffPlayed) {
+        var audioLiftOff = document.getElementById('audioLiftOff');
+        if (audioLiftOff) {
+          audioLiftOff.play().catch(e => console.log('Error playing Lift Off audio: ', e));
+          audioLiftOffPlayed = true;
+        }
+      }
+  
+      // Weiteres Scroll-Event für Wechsel zum Missions-Sound
+      $(window).on('scroll', function () {
+        var scrollPos = $(window).scrollTop();
+        if (!audioMissionPlayed && scrollPos >= 3000) {
+          var audioMission = document.getElementById('audioMission');
+          if (audioMission) {
+            $('#audioLiftOff').get(0).pause();
+            $('#audioLiftOff').get(0).currentTime = 0;
+            audioMission.play().catch(e => console.log('Error playing Mission audio: ', e));
+            audioMissionPlayed = true;
+          }
+        }
+      });
+    });
+  });
+  
