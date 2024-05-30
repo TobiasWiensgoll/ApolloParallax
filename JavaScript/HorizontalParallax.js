d71a267833c9f07370bfb4598b2bee7c40c4ed66
Tobias Wiensgoll <tobias.wiensgoll@gmail.com>
2024-05-30 18:02:22 +0200

    Szene2to3 Transition

---
+++ JavaScript/HorizontalParallax.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  let $cont = $(".container");
+  let $layer = $(".layer");
+
+  $cont.on("scroll", function () {
+    let X = $cont.scrollTop();
+
+    $layer.eq(1).css("left", X / 1.7 + "px");
+  });
+});
---
+++ JavaScript/transition.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+  const $window = $(window);
+  const $contentContainer = $(".ContentContainer");
+  const $newBackground = $(".Scene3");
+
+  $window.on("scroll", function () {
+    const scrollTop = $window.scrollTop();
+    const contentHeight = $contentContainer.outerHeight();
+    const windowHeight = $window.height();
+
+    if (scrollTop < contentHeight - windowHeight) {
+      $contentContainer.css("bottom", scrollTop * -1);
+      $newBackground.hide();
+      $(".rocket").removeClass("fadeOut");
+      $(".rocket").show();
+    } else {
+      // Wenn das Ende des ContentContainers erreicht ist
+      $contentContainer.css("bottom", (contentHeight - windowHeight) * -1);
+      $(".rocket").addClass("fadeOut");
+
+      // Neue Scroll-Effekt-Logik für den newBackground
+      $newBackground.show(); // Start animation
+    }
+  });
+});
