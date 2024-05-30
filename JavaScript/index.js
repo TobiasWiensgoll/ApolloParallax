0664ad520fdd114b5280bf9cfcabfc6374fc51d9
Tobias Wiensgoll <tobias.wiensgoll@gmail.com>
2024-05-30 12:52:33 +0200

    formatierung

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -36,47 +36,49 @@ $(document).ready(function () {
   simpleParallaxSidewards(4, 4, $(".satellite"));
   simpleParallaxSidewards(2, 2, $(".meteor"));
 
-  
-    // Setup initial volume from the slider
-    var volume = $('#volumeSlider').val();
-    $('audio').each(function() {
+  // Setup initial volume from the slider
+  var volume = $("#volumeSlider").val();
+  $("audio").each(function () {
+    this.volume = volume;
+  });
+
+  // Update volume on slider change
+  $("#volumeSlider").on("input", function () {
+    var volume = $(this).val();
+    $("audio").each(function () {
       this.volume = volume;
     });
-  
-    // Update volume on slider change
-    $('#volumeSlider').on('input', function() {
-      var volume = $(this).val();
-      $('audio').each(function() {
-        this.volume = volume;
-      });
-    });
-  
-    var audioLiftOffPlayed = false;
-    var audioMissionPlayed = false;
-  
-    // Initialisiert den Audio-Start bei der ersten Scroll-Aktion
-    $(window).one('scroll', function () {
-      if (!audioLiftOffPlayed) {
-        var audioLiftOff = document.getElementById('audioLiftOff');
-        if (audioLiftOff) {
-          audioLiftOff.play().catch(e => console.log('Error playing Lift Off audio: ', e));
-          audioLiftOffPlayed = true;
+  });
+
+  var audioLiftOffPlayed = false;
+  var audioMissionPlayed = false;
+
+  // Initialisiert den Audio-Start bei der ersten Scroll-Aktion
+  $(window).one("scroll", function () {
+    if (!audioLiftOffPlayed) {
+      var audioLiftOff = document.getElementById("audioLiftOff");
+      if (audioLiftOff) {
+        audioLiftOff
+          .play()
+          .catch((e) => console.log("Error playing Lift Off audio: ", e));
+        audioLiftOffPlayed = true;
+      }
+    }
+
+    // Weiteres Scroll-Event für Wechsel zum Missions-Sound
+    $(window).on("scroll", function () {
+      var scrollPos = $(window).scrollTop();
+      if (!audioMissionPlayed && scrollPos >= 3000) {
+        var audioMission = document.getElementById("audioMission");
+        if (audioMission) {
+          $("#audioLiftOff").get(0).pause();
+          $("#audioLiftOff").get(0).currentTime = 0;
+          audioMission
+            .play()
+            .catch((e) => console.log("Error playing Mission audio: ", e));
+          audioMissionPlayed = true;
         }
       }
-  
-      // Weiteres Scroll-Event für Wechsel zum Missions-Sound
-      $(window).on('scroll', function () {
-        var scrollPos = $(window).scrollTop();
-        if (!audioMissionPlayed && scrollPos >= 3000) {
-          var audioMission = document.getElementById('audioMission');
-          if (audioMission) {
-            $('#audioLiftOff').get(0).pause();
-            $('#audioLiftOff').get(0).currentTime = 0;
-            audioMission.play().catch(e => console.log('Error playing Mission audio: ', e));
-            audioMissionPlayed = true;
-          }
-        }
-      });
     });
   });
-  
+});
