02ce6efce1bd53b48f3f95716e8db24263d8bc65
Tobias Wiensgoll <tobias.wiensgoll@gmail.com>
2024-06-01 19:07:43 +0200

    navbar verbessert

--- JavaScript/navbar.js
+++ JavaScript/navbar.js
@@ -1,18 +1,28 @@
 $(document).ready(function () {
   const $body = $("body");
   let lastScroll = 0;
+  const $window = $(window);
+  const $contentContainer = $(".ContentContainer");
+  const $newBackground = $(".Scene3");
 
   $(window).on("scroll", () => {
+    const scrollTop = $window.scrollTop();
+    const contentHeight = $contentContainer.outerHeight();
+    const windowHeight = $window.height();
     let currentscroll = $(window).scrollTop();
 
-    if (currentscroll <= 0) {
-      $body.removeClass("scroll-up");
-    }
-    if (currentscroll > lastScroll && !$body.hasClass("scroll-down")) {
-      $body.removeClass("scroll-up").addClass("scroll-down");
-    }
-    if (currentscroll < lastScroll && $body.hasClass("scroll-down")) {
-      $body.removeClass("scroll-down").addClass("scroll-up");
+    if (scrollTop < contentHeight - windowHeight) {
+      if (currentscroll <= 0) {
+        $body.removeClass("scroll-up");
+      }
+      if (currentscroll > lastScroll && !$body.hasClass("scroll-down")) {
+        $body.removeClass("scroll-up").addClass("scroll-down");
+      }
+      if (currentscroll < lastScroll && $body.hasClass("scroll-down")) {
+        $body.removeClass("scroll-down").addClass("scroll-up");
+      }
+    } else {
+      $body.removeClass("scroll-down").removeClassClass("scroll-up");
     }
 
     lastScroll = currentscroll;
