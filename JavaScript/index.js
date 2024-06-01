2a6360766ea2e209b08ceaec048a122965599915
damian.sporin <damian.sporin@study.hs-duesseldorf.de>
2024-06-01 18:36:16 +0200

    Soundknopf statt Soundbar

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -27,7 +27,9 @@ function simpleParallaxSidewards(intensityX, intensityY, element) {
   });
 }
 
+
 $(document).ready(function () {
+  // Initialisierung der Parallax-Funktionen
   simpleParallax(4, $(".baloon"));
   simpleParallax(2, $(".airplane"));
   simpleParallaxHorizontal(4, $(".airplane"));
@@ -36,49 +38,46 @@ $(document).ready(function () {
   simpleParallaxSidewards(4, 4, $(".satellite"));
   simpleParallaxSidewards(2, 2, $(".meteor"));
 
-  // Setup initial volume from the slider
-  var volume = $("#volumeSlider").val();
-  $("audio").each(function () {
-    this.volume = volume;
-  });
-
-  // Update volume on slider change
-  $("#volumeSlider").on("input", function () {
-    var volume = $(this).val();
-    $("audio").each(function () {
-      this.volume = volume;
-    });
-  });
-
   var audioLiftOffPlayed = false;
   var audioMissionPlayed = false;
 
-  // Initialisiert den Audio-Start bei der ersten Scroll-Aktion
-  $(window).one("scroll", function () {
-    if (!audioLiftOffPlayed) {
-      var audioLiftOff = document.getElementById("audioLiftOff");
-      if (audioLiftOff) {
-        audioLiftOff
-          .play()
-          .catch((e) => console.log("Error playing Lift Off audio: ", e));
-        audioLiftOffPlayed = true;
-      }
+  // Behandlung des Checkbox-Status
+  $('#soundToggle').change(function() {
+    if (!this.checked) {
+      $('audio').each(function() {
+        this.pause();
+        this.currentTime = 0;  // Setzt den Sound zurück
+      });
+      audioLiftOffPlayed = false;
+      audioMissionPlayed = false;
     }
+  });
 
-    // Weiteres Scroll-Event für Wechsel zum Missions-Sound
-    $(window).on("scroll", function () {
-      var scrollPos = $(window).scrollTop();
+  $(window).on("scroll", function () {
+    var scrollPos = $(window).scrollTop();
+
+    if ($('#soundToggle').is(':checked')) {
+      // Starten des Lift Off Sounds
+      if (!audioLiftOffPlayed) {
+        var audioLiftOff = document.getElementById("audioLiftOff");
+        if (audioLiftOff) {
+          audioLiftOff.play().catch((e) => console.log("Error playing Lift Off audio: ", e));
+          audioLiftOffPlayed = true;
+        }
+      }
+
+      // Starten des Mission Sounds bei Erreichen von 3000px
       if (!audioMissionPlayed && scrollPos >= 3000) {
         var audioMission = document.getElementById("audioMission");
         if (audioMission) {
-          $("#audioLiftOff").get(0).pause();
-          $("#audioLiftOff").get(0).currentTime = 0;
-          audioMission
-            .play()
-            .catch((e) => console.log("Error playing Mission audio: ", e));
+          $("#audioLiftOff").get(0).pause();  // Pausieren des vorherigen Sounds
+          $("#audioLiftOff").get(0).currentTime = 0;  // Zurücksetzen des vorherigen Sounds
+          audioMission.play().catch((e) => console.log("Error playing Mission audio: ", e));
           audioMissionPlayed = true;
         }
       }
-    });
+    }
   });
 });
+
+  
