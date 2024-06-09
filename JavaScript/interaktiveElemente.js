dc126cc4d0838696c778bbc9190648fbe79d02f2
Katrin Schoene <katrin.schoene@study.hs-duesseldorf.de>
2024-06-09 17:46:17 +0200

    Vollbildbutton (dieses Mal müsste es passen)

--- JavaScript/interaktiveElemente.js
+++ JavaScript/interaktiveElemente.js
@@ -21,3 +21,32 @@ window.addEventListener('scroll', function() {
     var formattedTime = ('0' + hours).slice(-2) + ':' + ('0' + minutes).slice(-2);
     timerElement.textContent = formattedTime;
 });
+
+    var img = document.querySelector('.vollbildButton');
+
+    img.addEventListener('click', function() {
+        if (!document.fullscreenElement) {
+            // fullscreen starten
+            var docElm = document.documentElement;
+            if (docElm.requestFullscreen) {
+                docElm.requestFullscreen();
+            } else if (docElm.mozRequestFullScreen) { // Firefox
+                docElm.mozRequestFullScreen();
+            } else if (docElm.webkitRequestFullscreen) { // Chrome, Safari und Opera
+                docElm.webkitRequestFullscreen();
+            } else if (docElm.msRequestFullscreen) { // IE/Edge
+                docElm.msRequestFullscreen();
+            }
+        } else {
+            //fullscreen beenden
+            if (document.exitFullscreen) {
+                document.exitFullscreen();
+            } else if (document.mozCancelFullScreen) { // Firefox
+                document.mozCancelFullScreen();
+            } else if (document.webkitExitFullscreen) { // Chrome, Safari und Opera
+                document.webkitExitFullscreen();
+            } else if (document.msExitFullscreen) { // IE/Edge
+                document.msExitFullscreen();
+            }
+        }
+    });
