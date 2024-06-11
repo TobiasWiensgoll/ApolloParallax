f09f0091cdd130739a327c0455679fe24e550d65
Tobias Wiensgoll <tobias.wiensgoll@gmail.com>
2024-06-11 13:37:17 +0200

    Revert "Sound Button neu & Sounds funktionieren" hast parallax elemente gelöscht
    This reverts commit addf035a4a23444ec29236e72d42498e06391600.

--- JavaScript/sound.js
+++ JavaScript/sound.js
@@ -1,69 +1,53 @@
 $(document).ready(function () {
+    simpleParallax(4, $(".baloon"));
+    simpleParallax(2, $(".airplane"));
+    simpleParallaxHorizontal(4, $(".airplane"));
+    simpleParallax(2, $(".mountain"));
+    simpleParallaxSidewards(-2, 4, $(".ShootingStar"));
+    simpleParallaxSidewards(4, 4, $(".satellite"));
+    simpleParallaxSidewards(4, 4, $(".satelit"));
+    simpleParallaxSidewards(2, 2, $(".meteor"));
+  
     var audioLiftOff = document.getElementById("audioLiftOff");
     var audioMission = document.getElementById("audioMission");
-    var isSoundEnabled = false;
-    var audioMissionStarted = false;
-
-    var speakerButton = $('.sound-control img');
-
-    speakerButton.click(function () {
-        if (speakerButton.attr('src').includes('Lautsprecher_aus')) {
-            speakerButton.attr('src', 'Images/Lautsprecher_an.png');
-            isSoundEnabled = true;
-            audioMissionStarted = false;
-        } else {
-            speakerButton.attr('src', 'Images/Lautsprecher_aus.png');
-            audioLiftOff.pause();
-            audioMission.pause();
-            isSoundEnabled = false;
-            audioMissionStarted = false;
-        }
+    var isAudioPlaying = false;
+  
+    $('#soundToggle').change(function() {
+      if (!this.checked) {
+        audioLiftOff.pause();
+        audioMission.pause();
+        isAudioPlaying = false;
+      }
     });
-
-    $(window).on("scroll", debounce(function () {
-        var scrollPos = $(window).scrollTop();
-        if (isSoundEnabled) {
-            manageAudio(scrollPos);
-        }
-    }, 50));
-
+  
+    $(window).on("scroll", function () {
+      if ($('#soundToggle').is(':checked') && !isAudioPlaying) {
+        manageAudio($(window).scrollTop());
+      }
+    });
+  
     function manageAudio(scrollPos) {
-        if (scrollPos < 1500) {
-            setAudioPosition(audioLiftOff, scrollPos, 1500);
-            if (scrollPos >= 1500) {
-                audioLiftOff.pause(); // Pausiert den audioLiftOff bei 1000 Pixeln
-            }
-        } else {
-            if (!audioMissionStarted) {
-                audioMission.currentTime = 0;
-                audioMission.play();
-                audioMissionStarted = true;
-            }
-            audioLiftOff.pause(); // Sicherstellen, dass audioLiftOff pausiert ist, wenn audioMission spielt
-        }
+      isAudioPlaying = true;  // Setze, dass Audio gerade gespielt wird
+      if (scrollPos < 1000) {
+        setAudioPosition(audioLiftOff, scrollPos, 1000);
+        audioMission.pause();
+      } else {
+        setAudioPosition(audioMission, scrollPos - 1000, 4000);
+        audioLiftOff.pause();
+      }
     }
-
+  
     function setAudioPosition(audio, scrollPos, maxScroll) {
-        var duration = audio.duration;
-        var time = (scrollPos / maxScroll) * duration;
-        if (Math.abs(audio.currentTime - time) > 1) {
-            audio.currentTime = time;
-        }
-        if (audio.paused) {
-            audio.play();
-        }
+      // Berechne die Position im Audio basierend auf der Scroll-Position
+      var duration = audio.duration;
+      var time = (scrollPos / maxScroll) * duration;
+      if (audio.paused) {
+        audio.currentTime = time;
+        audio.play();
+      } else if (Math.abs(audio.currentTime - time) > 1) {
+        // Aktualisiere die Zeit nur, wenn die Differenz mehr als eine Sekunde beträgt
+        audio.currentTime = time;
+      }
     }
-
-    function debounce(func, wait, immediate) {
-        var timeout;
-        return function() {
-            var context = this, args = arguments;
-            clearTimeout(timeout);
-            timeout = setTimeout(function() {
-                timeout = null;
-                if (!immediate) func.apply(context, args);
-            }, wait);
-            if (immediate && !timeout) func.apply(context, args);
-        };
-    }
-});
+  });
+  
