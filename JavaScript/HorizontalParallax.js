8aa207ecefd3e7b3002b34226680b0a69f4617f9
Tobias Wiensgoll <tobias.wiensgoll@gmail.com>
2024-06-22 23:14:22 +0200

    endnames geaddet

--- JavaScript/HorizontalParallax.js
+++ JavaScript/HorizontalParallax.js
@@ -7,5 +7,6 @@ $(document).ready(function () {
 
     $layer.eq(1).css("left", X / 1.7 + "px");
     $layer.eq(3).css("left", X / -10 + "px");
+    $layer.eq(5).css("top", X / 20 + "vh");
   });
 });
